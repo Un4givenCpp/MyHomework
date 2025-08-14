84307e5796a5dfe782dabd598813d227e147dc9d
Un4given <alex345346@yandex.ru>
2025-08-14 12:36:27 +0300

    Added Lesson6

---
+++ lesson6/work6.1/work6.1.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <windows.h>
+
+#define MODE 1
+
+#ifndef MODE
+#error "Необходимо определить MODE"
+#endif
+
+#if MODE == 1
+int add(int a, int b) 
+{
+    return a + b;
+}
+#endif
+
+int main() 
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+#if MODE == 0
+    std::cout << "Работаю в режиме тренировки" << std::endl;
+
+#elif MODE == 1
+    std::cout << "Работаю в боевом режиме" << std::endl;
+
+    int num1, num2;
+    std::cout << "Введите число 1: ";
+    std::cin >> num1;
+    std::cout << "Введите число 2: ";
+    std::cin >> num2;
+
+    std::cout << "Результат сложения: " << add(num1,num2) << std::endl;
+
+#else
+    std::cout << "Неизвестный режим. Завершение работы" << std::endl;
+#endif
+
+    return EXIT_SUCCESS;
+}
---
+++ lesson6/work6.2/work6.2.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+// Определяем макрос SUB с правильной расстановкой скобок
+#define SUB(a, b) ((a) - (b))
+
+int main(int argc, char** argv)
+{
+    int a = 6;
+    int b = 5;
+    int c = 2;
+    std::cout << SUB(a, b) << std::endl;         // ((6) - (5)) = 1
+    std::cout << SUB(a, b) * c << std::endl;     // ((6) - (5)) * 2 = 1 * 2 = 2
+    std::cout << SUB(a, b + c) * c << std::endl; // ((6) - (5 + 2)) * 2 = (-1) * 2 = -2
+
+    return 0;
+}
